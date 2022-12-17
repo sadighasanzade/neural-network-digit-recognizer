0e71fbda645be1b1cc8961372ba8c51da3e51bf6
sadighasanzade <sadiqhesensr@gmail.com>
2022-12-17 21:11:42 +0400

    upload api added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Form, File
 from digit_detecter import Recognizer
+import shutil
 
 app = FastAPI()
 
@@ -8,4 +9,14 @@ async def read_item(path):
     recognizer = Recognizer(path= path)
     num = recognizer.recognize()
     return {"result ": str(num) }
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...), uuid : str = Form(...)):
+    with open(f"{uuid}.png", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)  
+    path = f"{uuid}.png"
+    recognizer = Recognizer(path=path)
+    num = recognizer.recognize()
+    return {"expression" : str(num)}
+    
 
